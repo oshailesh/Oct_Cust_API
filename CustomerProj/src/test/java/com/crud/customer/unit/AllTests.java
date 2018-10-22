707e058890a8b459991a6ffd06b1ae9360047cef
oshailesh <44256998+oshailesh@users.noreply.github.com>
2018-10-22 11:56:50 +1100

    new bcodebase

---
+++ CustomerProj/src/test/java/com/crud/customer/unit/AllTests.java
@@ -0,0 +1,11 @@
+package com.crud.customer.unit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({CustomerApplicationTests.class})
+public class AllTests {
+
+}
